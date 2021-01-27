5973def9d2aa3d8285945299e8bf6c23cc6629cd
ezgatrabajo <ezgatrabajo@gmail.com>
2021-01-27 18:48:30 -0300

    1 file modified

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -1,6 +1,16 @@
 import React, { Fragment, useState } from "react";
 
 const Contacto = () => {
+  const datos = [
+    { id: 1, nombre: "ReactJs" },
+    { id: 2, nombre: "VueJs" },
+    { id: 3, nombre: "AngularJs" }
+  ];
+
+  React.useEffect(() => {
+    console.log("use efect");
+  });
+
   return (
     <div>
       <h1>Contacto</h1>
