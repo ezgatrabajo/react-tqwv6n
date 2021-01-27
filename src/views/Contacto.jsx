162181300613515030dd8d4c505cd53de4176f90
ezgatrabajo <ezgatrabajo@gmail.com>
2021-01-27 19:00:52 -0300

    1 file modified

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -6,14 +6,30 @@ const Contacto = () => {
     { id: 2, nombre: "VueJs" },
     { id: 3, nombre: "AngularJs" }
   ];
+  const [equipo, setEquipo] = React.useState([]);
 
   React.useEffect(() => {
     console.log("use efect");
-  });
+    setEquipo(users);
+    obtenerDatos();
+    console.log(equipo);
+  }, []);
 
+  const obtenerDatos = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await data.json();
+    console.log(users);
+  };
   return (
     <div>
       <h1>Contacto</h1>
+      <ul>
+        {equipo.map(item => (
+          <li key="item.id">
+            {item.name} - {item.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
